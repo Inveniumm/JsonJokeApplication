35d1cfc6f0acaeb15a1bdee9777139ba4e92681c
Ugur Denli <ugur6@hotmail.co.uk>
2022-06-22 16:43:28 +0100

    final push
    final push

--- app/src/main/java/com/invenium/jsonjokeapplication/MainActivity.kt
+++ app/src/main/java/com/invenium/jsonjokeapplication/MainActivity.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.navHostFragment)
     }
-
 
 }
--- app/src/main/java/com/invenium/jsonjokeapplication/View/JokeFragment.kt
+++ app/src/main/java/com/invenium/jsonjokeapplication/View/JokeFragment.kt
@@ -4,42 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.fragment.app.Fragment
 import com.invenium.jsonjokeapplication.R
-import com.invenium.jsonjokeapplication.View.databinding.ActivityJokeFragmentBinding
-import com.invenium.jsonjokeapplication.databinding.ActivityJokeFragmentBinding
+import com.invenium.jsonjokeapplication.databinding.FragmentJokeBinding
+import kotlinx.android.synthetic.main.fragment_joke.*
+import java.io.File
 
-class JokeFragment : AppCompatActivity() {
+class JokeFragment : Fragment() {
 
-    private lateinit var binding: ActivityJokeFragmentBinding
+    private lateinit var binding: FragmentJokeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityJokeFragmentBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.jokeBtn.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
     }
 
-    fun getJoke() {
-        val b = R.id.jokeBtn as Button
-        binding.jokeBtn.setOnClickListener {
-            b.text.equals("I'm afraid for the calendar. Its days are numbered.")
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentJokeBinding.inflate(inflater, container, false)
+        return binding.root
 
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners(view)
+    }
 
+    private fun setListeners(v: View?){
+        val jsonString: String = File("./src/main/resources/joke.json").readText(Charsets.UTF_8)
 
-
+        jokeBtn.setOnClickListener {
+                tvJoke.text = (jsonString)
+            }
+    }
 }
